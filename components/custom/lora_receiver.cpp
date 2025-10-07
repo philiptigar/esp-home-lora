f1725e56be3933b974d4bc064421caf1fd7936c3
philiptigar <153817506+philiptigar@users.noreply.github.com>
2025-10-07 20:10:46 +0300

    Update lora_receiver.cpp

--- components/custom/lora_receiver.cpp
+++ components/custom/lora_receiver.cpp
@@ -1,50 +1,16 @@
-#ifndef LORA_RECEIVER_H
-#define LORA_RECEIVER_H
 
-#include "esphome.h"
-#include <SPI.h>
-#include <LoRa.h>
+#include "lora_receiver.h"
 
-class LoRaReceiver : public PollingComponent {
-public:
-  Sensor *sensor = nullptr;  // Pointer to the global sensor
-  
-  LoRaReceiver() : PollingComponent(2000) {} // 2 seconds interval
+extern Sensor &received_temperature; // Link to the global sensor
 
-  void setup() override {
-    ESP_LOGI("LoRa", "Initializing...");
-    LoRa.setPins(D8, D0, D2);  // CS, RST, DIO0 (IRQ)
-    if (!LoRa.begin(433E6)) {
-      ESP_LOGE("LoRa", "LoRa init failed");
-    }
-  }
+static auto my_lora = new LoRaReceiver();
 
-  void update() override {
-    int packetSize = LoRa.parsePacket();
-    if (packetSize) {
-      String incoming = "";
-      while (LoRa.available()) {
-        incoming += (char)LoRa.read();
-      }
-      ESP_LOGI("LoRa", "Received: %s", incoming.c_str());
-      
-      // Parse the incoming string to float
-      try {
-        float temp = std::stof(incoming.c_str());
-        publish_state(temp); // Call to publish the parsed temperature
-      } catch (...) {
-        ESP_LOGE("LoRa", "Invalid data");
-      }
-    }
-  }
+void setup() {
+  // Link the sensor pointer
+  my_lora->set_sensor(&received_temperature);
+  my_lora->setup(); // Call the setup of LoRa
+}
 
-  void set_sensor(Sensor *s) { sensor = s; } // linking the global sensor
-
-  void publish_state(float temp) {
-    if (sensor != nullptr) {
-      sensor->publish_state(temp);
-    }
-  }
-};
-
-#endif  // LORA_RECEIVER_H
+void loop() {
+  my_lora->update(); // Call update periodically
+}
